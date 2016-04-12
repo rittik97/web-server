b4739515a9bbbd19c51a82e19b4bcd7eb2345d44
rittik97 <rg2870@columbia.edu>
2016-04-11 22:56:24 -0400

    d

--- server.js
+++ server.js
@@ -14,18 +14,6 @@ app.use(bodyParser.json());
 //app.use(middleware.requireAuthentication);
 //app.get('/uber',funtion(req,res){});
 /*
-var url='https://api.uber.com/v1/estimates/price?server_token=f1nvxqcW3F031byA9PQv-a_3BaHFG_uj1WK-lo-I&start_latitude=40.798496&start_longitude=-73.964578&end_latitude=40.807552&end_longitude=-73.962573';
-
-request({
-		url: url,
-		json: true
-	}, function (error, response, body) {
-		if (error) {
-			console.log('Unable to fetch weather.');
-		} else {
-			console.log(body);
-		}
-	});
 */
 // Comment '/' all of it ? to go to index static file directly
 app.get('/about',middleware.requireAuthentication,function(req,res){
@@ -33,7 +21,29 @@ app.get('/about',middleware.requireAuthentication,function(req,res){
 });
 
 app.post('/tryme',function(req,res){
-	res.send(req.body.Sw);
+	var start_latitude=parseFloat(req.body.start_latitude);
+	var start_longitude=parseFloat(req.body.start_latitude);
+
+	var end_latitude=parseFloat(req.body.end_latitude);
+	var end_longitude=parseFloat(req.body.end_longitude);
+
+	var url='https://api.uber.com/v1/estimates/price?server_token=f1nvxqcW3F031byA9PQv-a_3BaHFG_uj1WK-lo-I&start_latitude=40.798496&start_longitude=-73.964578&end_latitude=40.807552&end_longitude=-73.962573';
+	var ubermesage;
+
+	request({
+			url: url,
+			json: true
+		}, function (error, response, body) {
+			if (error) {
+				console.log('Unable to fetch weather.');
+			} else {
+				//console.log(body);
+				ubermesage=body;
+			}
+		});
+
+
+	res.send(ubermesage);
 });
 
 app.use(express.static(__dirname + '/Public'));
