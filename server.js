02246d926275cc231c17aca58b7b8b582b37f2a8
rittik97 <rg2870@columbia.edu>
2016-04-13 22:10:17 -0400

    d

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/about',middleware.requireAuthentication,function(req,res){
 app.post('/tryme',function(req,res){
 
 	//
-	//req.body=JSON.stringify(req.body);
+	req.body=req.body.replace('''','');
 	console.log(req.body);
 	req.body=JSON.stringify(req.body);
 	console.log(req.body);
