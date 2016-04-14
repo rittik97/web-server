a3a0149d1cc54de9b006319d44289165b755d7b5
rittik97 <rg2870@columbia.edu>
2016-04-13 20:03:13 -0400

    d

--- server.js
+++ server.js
@@ -11,14 +11,55 @@ var PORT=process.env.PORT || 3000;  //Heroku gives port
 var middleware=require('./middleware.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+var returnmessage;
 //app.use(middleware.requireAuthentication);
 //app.get('/uber',funtion(req,res){});
 /*
 */
 // Comment '/' all of it ? to go to index static file directly
+
+
+/*
+var url='https://api.uber.com/v1/estimates/price?server_token=f1nvxqcW3F031byA9PQv-a_3BaHFG_uj1WK-lo-I&start_latitude=40.798496&start_longitude=-73.964578&end_latitude=40.807552&end_longitude=-73.962573';
+	var ubermesage;
+
+	request({
+			url: url,
+			json: true
+		}, function (error, response, body) {
+			if (error) {
+				console.log('Unable to fetch weather.');
+			} else {
+				ubermesage = body;
+				var ubertype1 = ubermesage.prices[0].display_name;
+				var price1 = ubermesage.prices[0].estimate;
+				var ubertype2 = ubermesage.prices[1].display_name;
+				var price2 = ubermesage.prices[1].estimate;
+				console.log("I can call you an "+ubertype1+" which will cost "+price1);
+				
+			}
+		});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/about',middleware.requireAuthentication,function(req,res){
 	res.send('yourguide.me');
 });
+
+
+
 
 app.post('/tryme',function(req,res){
 	var start_latitude=parseFloat(req.body.start_latitude);
@@ -37,13 +78,18 @@ app.post('/tryme',function(req,res){
 			if (error) {
 				console.log('Unable to fetch weather.');
 			} else {
-				console.log(body.prices(0));
-				ubermesage=body;
+				ubermesage = body;
+				var ubertype1 = ubermesage.prices[0].display_name;
+				var price1 = ubermesage.prices[0].estimate;
+				var ubertype2 = ubermesage.prices[1].display_name;
+				var price2 = ubermesage.prices[1].estimate;
+				returnmessage="I can call you an "+ubertype1+" which will cost "+price1;
+				console.log(returnmessage);
 			}
 		});
 
 
-	res.send(ubermesage);		
+	res.send(returnmessage);		
 
 });
 
